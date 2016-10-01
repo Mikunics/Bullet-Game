98b40aaf1f311acd025c4b622fb608f330fe34ec
walterlim <walterlim2001@yahoo.com>
2016-10-01 21:38:59 +0800

    Project Commit

--- Assets/TurretBehaviour.cs
+++ Assets/TurretBehaviour.cs
@@ -5,23 +5,37 @@ public class TurretBehaviour : MonoBehaviour {
 
     public GameObject projectile;
     public float speed = 10;
-    float timeStamp = Time.time + 0.5f;
+    float timeStamp = Time.time + 0.2f;
+
     void BulletFire()
-{
+    {
+            Rigidbody2D bullet;
+            this.transform.Rotate(0f, 0f, Random.Range(0f, 10.0f));
+            bullet = (Rigidbody2D)Instantiate(projectile.GetComponent<Rigidbody2D>(), this.transform.position, this.transform.rotation);
+            bullet.AddForce(bullet.transform.up * speed);
+            timeStamp = Time.time + 0.2f;
+    }
+
+    void Spray()
+    {
         Rigidbody2D bullet;
-        bullet = (Rigidbody2D) Instantiate(projectile.GetComponent<Rigidbody2D>(), this.transform.position, this.transform.rotation);
-        bullet.AddForce(bullet.transform.up * speed);
-        timeStamp = Time.time + 0.5f;
+        for (int i = 5; i > 0; i--)
+        {
+            this.transform.Rotate(0f, 0f, 10f);
+            bullet = (Rigidbody2D)Instantiate(projectile.GetComponent<Rigidbody2D>(), this.transform.position, this.transform.rotation);
+            bullet.AddForce(bullet.transform.up * speed);
+        }
+        timeStamp = Time.time + 0.2f;
     }
 void Start()
-{
-        BulletFire();
+{     
+        
 }
 
 // Update is called once per frame
 void Update()
 {
-        this.transform.Rotate(0f, 0f, 1f);
-        if(timeStamp <= Time.time) BulletFire();
-}
+        if (timeStamp <= Time.time) Spray(); 
+        
+    }
 }
