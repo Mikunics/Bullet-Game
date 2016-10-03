937a1cf1afe6b890955e3188b4c7a13522d0bc9d
walterlim <walterlim2001@yahoo.com>
2016-10-03 20:50:13 +0800

    Game State Manager
    Script for Game State Manager, currently has no working code but provides template for what should be done on each state

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour {
+
+    public enum GameState : short {Start, Store, Play, Options, Lose};
+    public GameState Game;
+
+void Start()
+{
+        Game = GameState.Start;
+}
+
+// Update is called once per frame
+void Update()
+{
+        switch (Game)
+        {
+            case GameState.Start:
+            // Start code goes here
+            break;
+
+            case GameState.Store:
+            // Store code goes here
+            break;
+
+            case GameState.Play:
+            // Play code goes here
+            break;
+
+            case GameState.Options:
+            // Options code goes here
+            break;
+
+            case GameState.Lose:
+            // Lose code goes here
+            break;
+
+            default:
+                Debug.Log("Game State Error, Invalid value in Game State");
+                break;
+        }
+}
+}
