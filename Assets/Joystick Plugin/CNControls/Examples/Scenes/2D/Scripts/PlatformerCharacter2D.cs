aca3a16caa5ac8f1308547a9d3de8ed6a3a30378
Aaron <hansaaron_d@yahoo.com>
2016-10-04 04:34:44 -0700

    Added Standard Assets
    Joystick assets

--- Assets/Joystick Plugin/CNControls/Examples/Scenes/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Joystick Plugin/CNControls/Examples/Scenes/2D/Scripts/PlatformerCharacter2D.cs
@@ -1,23 +1,21 @@
 using System;
 using UnityEngine;
 
-// Just in case so no "duplicate definition" stuff shows up
-namespace UnityStandardAssets.Copy._2D
+namespace UnityStandardAssets._2D
 {
     public class PlatformerCharacter2D : MonoBehaviour
     {
-        [SerializeField]
-        private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-        [SerializeField]
-        private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-        [SerializeField]
-        private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
-        [SerializeField]
-        private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+        [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+        [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
+        private Transform m_CeilingCheck;   // A position marking where to check for ceilings
+        const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
@@ -26,6 +24,7 @@ namespace UnityStandardAssets.Copy._2D
         {
             // Setting up references.
             m_GroundCheck = transform.Find("GroundCheck");
+            m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
@@ -44,19 +43,38 @@ namespace UnityStandardAssets.Copy._2D
                     m_Grounded = true;
             }
             m_Anim.SetBool("Ground", m_Grounded);
+
+            // Set the vertical animation
+            m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
         }
 
 
-        public void Move(float move, bool jump)
+        public void Move(float move, bool crouch, bool jump)
         {
+            // If crouching, check to see if the character can stand up
+            if (!crouch && m_Anim.GetBool("Crouch"))
+            {
+                // If the character has a ceiling preventing them from standing up, keep them crouching
+                if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+                {
+                    crouch = true;
+                }
+            }
+
+            // Set whether or not the character is crouching in the animator
+            m_Anim.SetBool("Crouch", crouch);
+
             //only control the player if grounded or airControl is turned on
             if (m_Grounded || m_AirControl)
             {
+                // Reduce the speed if crouching by the crouchSpeed multiplier
+                move = (crouch ? move*m_CrouchSpeed : move);
+
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 m_Anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight)
@@ -64,7 +82,7 @@ namespace UnityStandardAssets.Copy._2D
                     // ... flip the player.
                     Flip();
                 }
-                // Otherwise if the input is moving the player left and the player is facing right...
+                    // Otherwise if the input is moving the player left and the player is facing right...
                 else if (move < 0 && m_FacingRight)
                 {
                     // ... flip the player.
